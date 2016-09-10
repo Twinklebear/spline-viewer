6b69ece1ff59dd0ee3860abb4031e88d4c5c80d5
Will Usher <willusher.life@gmail.com>
2016-09-10 10:03:34 -0600

    Add basic glium start app, displays a red window

--- programming/pa1/src/main.rs
+++ programming/pa1/src/main.rs
@@ -1,3 +1,28 @@
+#[macro_use]
+extern crate glium;
+
+use glium::{DisplayBuild, Surface, glutin};
+
 fn main() {
-    println!("Hello, world!");
+
+    let display = glutin::WindowBuilder::new()
+        .with_dimensions(1024, 720)
+        .with_title("CS6670 Programming Assignment 1 - Will Usher")
+        .with_vsync()
+        .build_glium().unwrap();
+    // TODO: Use build_glium_debug on debug builds
+
+    'outer: loop {
+        for e in display.poll_events() {
+            match e {
+                glutin::Event::Closed => break 'outer,
+                _ => {}
+            }
+        }
+
+        let mut target = display.draw();
+        target.clear_color(1.0, 0.0, 0.0, 0.0);
+        target.finish().unwrap();
+    }
 }
+
