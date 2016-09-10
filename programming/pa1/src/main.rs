88cc54d7bb8f52401a6408fd02b1ffcfe3c490e9
Will Usher <willusher.life@gmail.com>
2016-09-10 14:15:12 -0600

    Adding ImGui

--- programming/pa1/src/main.rs
+++ programming/pa1/src/main.rs
@@ -1,7 +1,10 @@
 #[macro_use]
 extern crate glium;
+#[macro_use]
+extern crate imgui;
 
 use glium::{DisplayBuild, Surface, glutin};
+use imgui::ImGui;
 
 fn main() {
 
@@ -12,6 +15,11 @@ fn main() {
         .build_glium().unwrap();
     // TODO: Use build_glium_debug on debug builds
 
+    let mut imgui = ImGui::init();
+    let mut imgui_renderer = imgui::glium_renderer::Renderer::init(&mut imgui, &display).unwrap();
+
+    // TODO: Setup key bindings for imgui
+
     'outer: loop {
         for e in display.poll_events() {
             match e {
@@ -21,7 +29,19 @@ fn main() {
         }
 
         let mut target = display.draw();
-        target.clear_color(1.0, 0.0, 0.0, 0.0);
+        target.clear_color(0.2, 0.2, 0.2, 1.0);
+
+        let window = display.get_window().unwrap();
+        let size_pts = window.get_inner_size_points().unwrap();
+        let size_pixels = window.get_inner_size_pixels().unwrap();
+        let ui = imgui.frame(size_pts, size_pixels, 0.16);
+        ui.window(im_str!("This is a programming assignment"))
+            .size((300.0, 100.0), imgui::ImGuiSetCond_FirstUseEver)
+            .build(|| {
+                ui.text(im_str!("some ui text"));
+            });
+        imgui_renderer.render(&mut target, ui).unwrap();
+
         target.finish().unwrap();
     }
 }
