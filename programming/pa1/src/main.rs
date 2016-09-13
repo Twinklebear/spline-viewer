02ecaeb2a459bbab5f32d325c60d63dda0e9868f
Will Usher <willusher.life@gmail.com>
2016-09-13 09:45:58 -0600

    Show framerate and GL version info in control panel

--- programming/pa1/src/main.rs
+++ programming/pa1/src/main.rs
@@ -126,7 +126,9 @@ fn main() {
         .with_title("CS6670 Programming Assignment 1 - Will Usher")
         .with_vsync()
         .build_glium().unwrap();
-    // TODO: Use build_glium_debug on debug builds
+
+    println!("Got OpenGL: {:?}", display.get_opengl_version());
+    println!("Got GLSL: {:?}", display.get_supported_glsl_version());
 
     let mut imgui = ImGuiSupport::init();
     let mut imgui_renderer = imgui::glium_renderer::Renderer::init(&mut imgui.imgui, &display).unwrap();
@@ -152,10 +154,15 @@ fn main() {
         target.clear_color(0.2, 0.2, 0.2, 1.0);
 
         let ui = imgui.render_ui(&display);
-        ui.window(im_str!("This is a programming assignment"))
+        ui.window(im_str!("Control Panel"))
             .size((300.0, 100.0), imgui::ImGuiSetCond_FirstUseEver)
             .build(|| {
-                ui.text(im_str!("some ui text"));
+                let fps = ui.framerate();
+                let gl_version = display.get_opengl_version();
+                let glsl_version = display.get_supported_glsl_version();
+                ui.text(im_str!("Framerate: {:.3} FPS ({:.3} ms)", fps, 1000.0 / fps));
+                ui.text(im_str!("OpenGL Version: {}.{}", gl_version.1, gl_version.2));
+                ui.text(im_str!("GLSL Version: {}.{}", glsl_version.1, glsl_version.2));
             });
         imgui_renderer.render(&mut target, ui).unwrap();
 
